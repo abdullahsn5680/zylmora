e07d9bb0f467a066a465813cc7cb6a612475b216
Abdul Ahad <your-email@example.com>
2025-07-09 14:23:31 +0500

    Updated caching behavior / latest changes

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,26 @@ export const revalidate = 3600;
 
 import Banner from './Components/UI/Banner';
 import CardContainer from './Components/UI/Container/CardContainer';
+import { headers } from 'next/headers';
+async function getData() {
+  const time = new Date().toLocaleTimeString();
+  console.log(`🧠 [Server] Page rendered at: ${time}`);
+
+  // Get headers to detect current host
+  const host = headers().get('host');
+  const protocol = host?.includes('localhost') ? 'http' : 'https';
+  const baseUrl = `${protocol}://${host}`;
+
+  const res = await fetch(`${baseUrl}/api/getHome`, {
+    next: { revalidate: 3600 },
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
 
 export default async function HomePage() {
   const content = await getData();
